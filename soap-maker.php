59178262f73fed9e15d0b097d233ccf59e1c8fa2
Thomas Hansen <thomas@spysystem.dk>
2018-09-24 23:43:55 +0200

    - Changed usage - Added support for Basic Authentication

--- soap-maker.php
+++ soap-maker.php
@@ -6,21 +6,36 @@ require 'vendor/autoload.php';
  */
 class SoapMaker
 {
-	const OutputFolder = '/output/';
+	private const OutputFolder = '/output/';
+
+	private const Option_ProjectName	= 'project-name';
+	private const Option_WSDLPath		= 'wsdl-path';
+	private const Option_Username		= 'username';
+	private const Option_Password		= 'password';
+
+	private const Option_WithValue		= ':';
 
 	private $strProjectName;
 	private $strWSDL;
+	private $strUsername;
+	private $strPassword;
 
 	/**
 	 * SoapMaker constructor.
 	 *
-	 * @param string $strProjectName
-	 * @param string $strWSDL
+	 * @param array $arrOptions
 	 */
-	public function __construct(string $strProjectName, string $strWSDL)
+	public function __construct(array $arrOptions)
 	{
-		$this->strProjectName	= $strProjectName;
-		$this->strWSDL			= $strWSDL;
+		try
+		{
+			$this->parseOptions($arrOptions);
+		}
+		catch(Exception $oException)
+		{
+			echo $oException->getMessage()."\n";
+			$this->showUsage();
+		}
 	}
 
 	public function generate(): void
@@ -35,6 +50,20 @@ class SoapMaker
 			}
 			mkdir($strSrcOutputDir, 0777, true);
 
+			$arrSoapClientOptions	= [
+				'trace'        => true,
+				'exceptions'   => true,
+				'soap_version' => SOAP_1_2,
+				'encoding'     => 'UTF-8'
+			];
+
+			if($this->strUsername !== '')
+			{
+				$arrSoapClientOptions['authentication']	= SOAP_AUTHENTICATION_BASIC;
+				$arrSoapClientOptions['login']			= $this->strUsername;
+				$arrSoapClientOptions['password']		= $this->strPassword;
+			}
+
 			$oGenerator	= new \Wsdl2PhpGenerator\Generator();
 			$oGenerator->generate(
 				new \Wsdl2PhpGenerator\Config([
@@ -42,12 +71,7 @@ class SoapMaker
 					'outputDir'			=> $strSrcOutputDir,
 					'namespaceName'		=> $this->strProjectName,
 					'bracketedArrays'	=> true,
-					'soapClientOptions'	=> [
-						'trace'        => true,
-						'exceptions'   => true,
-						'soap_version' => SOAP_1_2,
-						'encoding'     => 'UTF-8'
-					]
+					'soapClientOptions'	=> $arrSoapClientOptions
 				])
 			);
 
@@ -65,15 +89,16 @@ class SoapMaker
 	}
 
 
-	public static function ShowUsage(): void
+	private function showUsage(): void
 	{
 		echo <<<EOT
 Usage:
-	php soap-maker.php <ProjectName> <WSDL>
+	php soap-maker.php --project-name <ProjectName> --wsdl-path <WSDL> [--username <Username> --password <Password>]
 
 Where:
 	<ProjectName> = Name for the project, without spaces
 	<WSDL> = file or URL for the WSDL SOAP description
+	<Username>, <Password> = credentials for Basic Authentication, if required
 
 Project will be generated into the "output" folder
 
@@ -117,14 +142,54 @@ EOT;
 EOT;
 
 	}
+
+	/**
+	 * @param $arrOptions
+	 * @throws Exception
+	 */
+	private function parseOptions($arrOptions): void
+	{
+		if(!array_key_exists(self::Option_ProjectName, $arrOptions) || $arrOptions[self::Option_ProjectName] === '')
+		{
+			throw new Exception('Missing Project Name!');
+		}
+
+		if(!array_key_exists(self::Option_WSDLPath, $arrOptions) || $arrOptions[self::Option_WSDLPath] === '')
+		{
+			throw new Exception('Missing wsdl url or filename!');
+		}
+
+		if(
+			(
+				array_key_exists(self::Option_Username, $arrOptions)
+			 &&	!array_key_exists(self::Option_Password, $arrOptions)
+			)
+		 ||
+			(
+				!array_key_exists(self::Option_Username, $arrOptions)
+			 &&	array_key_exists(self::Option_Password, $arrOptions)
+			)
+		)
+		{
+			throw new Exception('To use authentication, you must provide both Username and Password!');
+		}
+		$this->strProjectName	= $arrOptions[self::Option_ProjectName];
+		$this->strWSDL			= $arrOptions[self::Option_WSDLPath];
+		$this->strUsername		= $arrOptions[self::Option_Username] ?? '';
+		$this->strPassword		= $arrOptions[self::Option_Password] ?? '';
+	}
+
+	public static function GetLongOptsArray(): array
+	{
+		return [
+			self::Option_ProjectName.self::Option_WithValue,
+			self::Option_WSDLPath.self::Option_WithValue,
+			self::Option_Username.self::Option_WithValue,
+			self::Option_Password.self::Option_WithValue
+		];
+	}
 }
 
-if($argc !== 3)
-{
-	SoapMaker::ShowUsage();
-	exit();
-}
-
-$oSoapMaker	= new SoapMaker($argv[1], $argv[2]);
+$oSoapMaker	= new SoapMaker(getopt('', SoapMaker::GetLongOptsArray()));
 
 $oSoapMaker->generate();
